286cce9b8d2bb28775502040e1b299fbaa8767e6
Warren Wright <warren.wright@pathlab.co.nz>
2023-05-31 20:52:23 +1200

    New switch implimented --type to query csv from NZPOCS or a json dump from the nzhts

--- nzpocs.py
+++ nzpocs.py
@@ -1,12 +1,14 @@
 import csv
 import argparse
 import os 
+import json
 
 # parse an argument to define if operating on lis or eclair data and pass in the path for the input file
 parser = argparse.ArgumentParser()
 parser.add_argument("--inputfile", "-f", required=True)
 parser.add_argument("--outputfile", "-o", required=True)
 parser.add_argument("--source","-s", choices=['lis','eclair'])
+parser.add_argument("--type","-t", default="csv", choices=['csv','json'])
 args = parser.parse_args()
 
 def search_nzpocs(search_text, search_in_text):
@@ -28,9 +30,17 @@ def search_nzpocs(search_text, search_in_text):
     return False
 
 def main(): 
-    # Open the first CSV file containing the codeset relative to nzpocs folder
-    with open('nzpocs-observation-code-set-1-October-2022.csv', newline='', encoding='utf-8') as csvfile1:
-        nzpocs = csv.DictReader(csvfile1)
+    # Open the CSV or json file containing the codeset relative to nzpocs folder
+    if args.type == 'csv':
+        pocsfname = 'nzpocs-observation-code-set-1-October-2022.csv'
+    if args.type == 'json':
+        pocsfname = 'nzhts-nzpocs.json'
+
+    with open(pocsfname, newline='', encoding='utf-8') as nzpocsfile:
+        if args.type == 'csv':
+            nzpocs = csv.DictReader(nzpocsfile)
+        if args.type == 'json':
+            nzpocs = json.load(nzpocsfile)
         
         # Open the second CSV file containing the test groups from the passed in argument
         with open(os.path.abspath(args.inputfile), newline='', encoding='utf-8') as csvfile2:
@@ -49,25 +59,46 @@ def main():
                 matches = 0
                 #print(search_text)
                 # Loop through each row in the codeset CSV file
-                for nzpoc in nzpocs:
-                    # Extract the text to search within from the relevant columns
-                    search_in_text = [nzpoc['COMPONENT'],nzpoc['NZ_SHORT_NAME']]
-                    #print(search_in_text)
-                    if search_nzpocs(search_text,search_in_text):
-                        matches+=1
+                if args.type == 'csv':
+                    for nzpoc in nzpocs:
+                        # Extract the text to search within from the relevant columns
+                        search_in_text = [nzpoc['COMPONENT'],nzpoc['NZ_SHORT_NAME']]
+                        #print(search_in_text)
+                        if search_nzpocs(search_text,search_in_text):
+                            matches+=1
+                            if args.source == "lis":
+                                matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'], nzpoc['CODE'],nzpoc['NZ_SHORT_NAME'],nzpoc['COMPONENT'],matches))
+                            else:
+                                matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'', nzpoc['CODE'],nzpoc['NZ_SHORT_NAME'],nzpoc['COMPONENT'],matches))
+                        # Reset the codeset CSV reader to the beginning of the file
+                    if matches == 0:
                         if args.source == "lis":
-                            matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'], nzpoc['CODE'],nzpoc['NZ_SHORT_NAME'],nzpoc['COMPONENT'],matches))
+                            matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'],'','','','0'))
                         else:
-                            matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'', nzpoc['CODE'],nzpoc['NZ_SHORT_NAME'],nzpoc['COMPONENT'],matches))
-                    # Reset the codeset CSV reader to the beginning of the file
-                if matches == 0:
-                    if args.source == "lis":
-                        matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'],'','','','0'))
-                    else:
-                        matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'','','','','0'))
-                csvfile1.seek(0)
-                nzpocs = csv.DictReader(csvfile1)
-                        
+                            matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'','','','','0'))
+                    nzpocsfile.seek(0)
+                    nzpocs = csv.DictReader(nzpocsfile)
+                if args.type == 'json':
+                    nzpocs_array = nzpocs['expansion']['contains']
+                    #print(str(nzpocs_array))
+                    for item in nzpocs_array:
+                        for key,value in item.items():
+                            if key == 'code':
+                                curr_code = value
+                            if key == 'display':
+                                search_in_text = [value]
+                        if search_nzpocs(search_text,search_in_text):
+                            matches+=1
+                            if args.source == "lis":
+                                matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'], curr_code,value,matches))
+                            else:
+                                matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'', curr_code,value,matches))
+                    if matches == 0:
+                        if args.source == "lis":
+                            matching_data.append((paltest['test'], paltest['short_desc'],paltest['text'],'','','','0'))
+                        else:
+                            matching_data.append((paltest['OBSC_OBSID'], paltest['OBSC_DESC'],'','','','','0'))
+                                            
             # Print the list of matching data
             with open(os.path.abspath(args.outputfile),'w', newline='', encoding='utf-8') as csvfilew:
                 matchwriter = csv.writer(csvfilew, dialect='excel')
